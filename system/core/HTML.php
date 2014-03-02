0a8e5707f1230cc79efd4d196d186e7a3a856ef8
SprayHank <fantasyoui@gmail.com>
2014-03-02 21:07:01 +0800

    添加文件

---
+++ system/core/HTML.php
@@ -0,0 +1,14 @@
+<?PHP defined('IN_SPD') || die('No direct script access.');
+
+
+
+class SPD_HTML {
+
+    function ddd() { }
+    function doc_type() {
+        return '<!DOCTYPE html><html><head><meta charset="UTF-8" />';
+    }
+    function render() {
+        return '</body></html>';
+    }
+}
---
+++ system/initialization.php
@@ -0,0 +1,3 @@
+<?PHP defined('IN_SPD') || die('No direct script access.');
+
+function_exists('set_magic_quotes_runtime') && set_magic_quotes_runtime(0);
